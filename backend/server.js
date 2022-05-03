a1913582e18d97d65645c0d0af7634e89cb5dc33
saduni <saduniudeshika0@gmail.com>
2022-05-03 09:37:23 +0530

    Create credit card payment api

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var cors = require("cors");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const stripeRoute = require("./routes/stripe");
+
 app.use(cors());
+
+app.use("/api/checkout", stripeRoute);
 
 const port = process.env.PORT || 3000;
 
