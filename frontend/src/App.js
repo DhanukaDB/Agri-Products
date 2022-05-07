b31f984153eb4f63f691b6c32efe02c33d242337
saduni <saduniudeshika0@gmail.com>
2022-05-07 21:46:02 +0530

    Create checkout button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 
+import PaymentForm from "./components/PaymentForm";
 function App() {
   return (
     <div className="App">
-      <h1>Goviyek</h1>
-</div>
+      <PaymentForm />
+    </div>
   );
 }
 
