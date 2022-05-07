2a638e4a7f737631196653346a97ddae8e8c350c
DhanukaDB <it20207236@my.sliit.lk>
2022-05-07 16:29:10 +0530

    Create home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Goviyek</h1>
-</div>
+    <Router>
+      <div>
+        <Routes>
+        <Route path = "/" element={<Home />}></Route>
+
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
