47435940607f9ce81ee3ca49ec66d973785c9975
Yasara19 <86340688+Yasara19@users.noreply.github.com>
2022-05-08 12:42:10 +0530

    Add Customer backend

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,34 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const stripeRoute = require("./routes/stripe");
-
 app.use(cors());
 
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+             console.log("Mongodb connection success!");
+
+})
+//stripe router
+const stripeRoute = require("./routes/stripe");
 app.use("/api/checkout", stripeRoute);
+
+//Customer router
+const customerRouter = require("./routes/customers.js");
+app.use("/customer", customerRouter);
 
 const port = process.env.PORT || 3000;
 
