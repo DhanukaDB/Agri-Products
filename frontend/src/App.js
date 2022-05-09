0d3fe13697eda095c534c64048eb4731ba483450
saduni <saduniudeshika0@gmail.com>
2022-05-09 09:40:08 +0530

    Track files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
+import "./index.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./pages/home";
+import Payment from "./components/Payment/Payment";
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/payment" element={<Payment />}></Route>
         </Routes>
       </div>
     </Router>
