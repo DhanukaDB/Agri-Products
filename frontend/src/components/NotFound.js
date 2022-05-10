c921ebc13c3327dfdbe6ced9b4751b27dbb59d4a
saduni <saduniudeshika0@gmail.com>
2022-05-10 14:53:33 +0530

    Add Checkout not found component

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="contaier">
+      <h3>Page Not found</h3>
+      <h6>404</h6>
+    </div>
+  );
+};
+
+export default NotFound;
