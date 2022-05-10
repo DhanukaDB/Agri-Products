bd7cae9433d98981502aa99fc82c26d60c196f53
saduni <saduniudeshika0@gmail.com>
2022-05-10 08:47:16 +0530

    Create review order section

--- frontend/src/components/Payment/Payment.js
+++ frontend/src/components/Payment/Payment.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "./payment.css";
+import { Button } from "react-bootstrap";
 
 function Payment() {
   return (
-    <div>
-      <div className="container">
+    <div className="main">
+      <div className="container-review">
         <h2>Review Your order</h2>
         <div className="container-address">
           <h5>Shipping Address</h5>
-          <div>
+          <div className="address-details">
             <p>Full Nmame:</p>
             <p>Flat:</p>
             <p>Area:</p>
@@ -18,6 +20,15 @@ function Payment() {
         </div>
         <div className="payment-container">
           <h5>Payment Method</h5>
+          <Button className="pay-button" variant="outline-dark">
+            Pay Via Credit Card
+          </Button>
+          <Button className="pay-button" variant="dark">
+            Pay Via Mobile Phone
+          </Button>
+        </div>
+        <div className="order">
+          <h5>Your order</h5>
         </div>
       </div>
       <div className="subtotal-container"></div>
