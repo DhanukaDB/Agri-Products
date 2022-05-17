4a165046104673d6347b548fad6a97ac300aec91
saduni <saduniudeshika0@gmail.com>
2022-05-17 18:40:21 +0530

    Create Mail.js component

---
+++ frontend/src/components/Mail.js
@@ -0,0 +1,33 @@
+import React from "react";
+import emailjs from "emailjs-com";
+
+function Mail() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_7yavpge",
+        "template_motceg4",
+        e.target,
+        "fy8SgBtOKzkGQmN-T"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }
+  return (
+    <div>
+      <form onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <input type="submit" value="send" />
+      </form>
+    </div>
+  );
+}
+
+export default Mail;
